ee10c290c5a58bd25bde3da00d1ca3dae66d78de
unknown <vikram@nandi.net>
2023-02-05 14:19:26 -0800

    FULL IMPLEMENTATION BABYYYY

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,14 +4,19 @@
 
 package frc.robot.commands;
 
+import frc.robot.commands.Drivetrain.SwerveDriveOdometry;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public final class Autos {
   /** Example static factory for an autonomous command. */
   public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-    return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+    return Commands.sequence(new SwerveDriveOdometry(new Pose2d(new Translation2d(0, -17.16667), new Rotation2d(0))), new WaitCommand(1), new SwerveDriveOdometry(new Pose2d(new Translation2d(0, 0), new Rotation2d(0))), new WaitCommand(1), new SwerveDriveOdometry(new Pose2d(new Translation2d(-17.16667, 0), new Rotation2d(0))));
   }
 
   private Autos() {
