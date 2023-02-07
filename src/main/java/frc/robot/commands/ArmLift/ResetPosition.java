9e60a8908893c69b6db1757b45dfac55227073e9
RayirthDinesh <91096595+RayirthDinesh@users.noreply.github.com>
2023-02-06 17:51:32 -0800

    requires a shaz check

--- src/main/java/frc/robot/commands/ArmLift/ResetPosition.java
+++ src/main/java/frc/robot/commands/ArmLift/ResetPosition.java
@@ -7,6 +7,7 @@ public class ResetPosition extends CommandBase {
   double resetPosition;
   boolean reset = false;
   double increment;
+  int state=1;
   public ResetPosition() {
     
     // Use addRequirements() here to declare subsystem dependencies.
@@ -18,7 +19,10 @@ public class ResetPosition extends CommandBase {
   public void initialize() {
     RobotContainer.m_armLift.setPos(0);
     RobotContainer.m_armLift.movePos(0);
-    RobotContainer.m_armLift.movePos(2048*100);
+    
+
+ 
+    
     
     
     //   if(RobotContainer.m_armLift.getLimitSwitch()){
@@ -42,12 +46,49 @@ public class ResetPosition extends CommandBase {
   
   @Override
   public void execute() {
-    
-    if(RobotContainer.m_armLift.getLimitSwitch()){
-      resetPosition=RobotContainer.m_armLift.getPos();
-      RobotContainer.m_armLift.movePos(resetPosition-10000);
-      RobotContainer.m_armLift.movePos(resetPosition);
+    increment+=1;
+    if(state==1){
+      RobotContainer.m_armLift.movePos(increment*1000);
+      System.out.println("state 1");
+      if(RobotContainer.m_armLift.getLimitSwitch()){
+        state=2;
+      }
+      
     }
+    if(RobotContainer.m_armLift.getLimitSwitch()&&state==2){
+      increment=0;
+      resetPosition=RobotContainer.m_armLift.getPos();
+      RobotContainer.m_armLift.movePos(resetPosition-20000);
+      System.out.println("state 2");  
+      if(!RobotContainer.m_armLift.getLimitSwitch()){
+        state=3;
+        System.out.println("state 3");
+        
+      }
+    }
+    if(state==3){
+      increment+=1;
+      RobotContainer.m_armLift.movePos(increment*1000);
+      System.out.println("state 3");
+      state=4;
+    }
+    // if(RobotContainer.m_armLift.getPos()==resetPosition-10000){
+    //   System.out.println("inside");
+    //   RobotContainer.m_armLift.movePos(resetPosition);
+    //   if(RobotContainer.m_armLift.getLimitSwitch()){
+    //     reset=true;
+    //   }
+    
+      // if(RobotContainer.m_armLift.getLimitSwitch()){
+      //   reset=true;
+      // }
+      //RobotContainer.m_armLift.movePos(resetPosition);
+      
+     
+
+
+  
+  
   }
 
     //RobotContainer.m_armLift.speedControl(0.6);
@@ -62,8 +103,11 @@ public class ResetPosition extends CommandBase {
   public void end(boolean interrupted) {
     //RobotContainer.m_armLift.changeVelocity(100000);
     //RobotContainer.m_armLift.speedControl(0);
+    //RobotContainer.m_armLift.movePos(resetPosition);
+    RobotContainer.m_armLift.setPos(0);
+    RobotContainer.m_armLift.movePos(0);
     
-    //RobotContainer.m_armLift.setPos(0);
+    
     //RobotContainer.m_armLift.movePos(RobotContainer.m_armLift.getPos());
     
 
@@ -84,6 +128,9 @@ public class ResetPosition extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return reset;
+    if(state==4){
+      return true;
+    }
+    return false;
   }
 }
